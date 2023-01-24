4cebe3773cf44980c02c2d52f41443f7f7b41619
kelseyjking <kking11@chicagobooth.edu>
2023-01-24 23:31:15 +0000

    got decimals to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square
-    @num = params.fetch("elephant").to_i
+    @num = params.fetch("elephant").to_f
     @square_of_num = @num ** 2
   render({ :template => "calculation_templates/square_results.html.erb"})
   end
