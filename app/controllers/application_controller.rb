6b74d71368e9e82d13b96dc6ce67933e9c453c6c
kelseyjking <kking11@chicagobooth.edu>
2023-01-25 03:49:17 +0000

    97% there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,41 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
   render({ :template => "calculation_templates/square_results.html.erb"})
   end
+
+  def blank_random
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("minimum").to_f
+    @upper = params.fetch("maximum").to_f
+    @result = rand(@lower..@upper)
+  render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
+  def blank_sqrt
+    render({ :template => "calculation_templates/sqrt_form.html.erb"})
+  end
+
+  def calculate_sqrt
+    @inputnum = params.fetch("user_number").to_f
+    @square_root = Math.sqrt(@inputnum)
+  render({ :template => "calculation_templates/sqrt_results.html.erb"})
+  end
+
+  def blank_pmt
+    render({ :template => "calculation_templates/pmt_form.html.erb"})
+  end
+
+  def calculate_pmt
+    @aprnum = params.fetch("user_apr").to_f
+    @yearsnum = params.fetch("user_years").to_f
+    @principalnum = params.fetch("user_pv").to_f
+    @pmt_num = @principalnum * @yearsnum * @aprnum
+    @ratepermonth = @aprnum / 12 / 100
+    @denominator = 1-(1+@ratepermonth)**(-12*5)
+    @monthlypmtresult = @ratepermonth*@principalnum/@denominator
+  render({ :template => "calculation_templates/pmt_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ get("/square/new", {:controller => "application", :action => "blank_square_form"
 
 get("/square/results", {:controller => "application", :action => "calculate_square"})
 
+get("/random/new", {:controller => "application", :action => "blank_random"})
+
+get("/random/results", {:controller => "application", :action => "calculate_random"})
+
+get("/square_root/new", {:controller => "application", :action => "blank_sqrt"})
+
+get("/square_root/results", {:controller => "application", :action => "calculate_sqrt"})
+
+get("/payment/new", {:controller => "application", :action => "blank_pmt"})
+
+get("/payment/results", {:controller => "application", :action => "calculate_pmt"})
+
 end
